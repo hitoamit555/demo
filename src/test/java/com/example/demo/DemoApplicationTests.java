5f737dfc9bf2071cb1c26ab95d230dfaec14c180
localadmin <localadmin@localadmins-MacBook-Pro-2.local>
2018-12-05 15:36:07 +0530

    Adding Testcase

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,16 +1,27 @@
 package com.example.demo;
 
+import com.example.demo.controller.DemoController;
+import javafx.beans.binding.When;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.validation.constraints.AssertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
+	@InjectMocks
+	DemoController demoController;
+
 	@Test
 	public void contextLoads() {
+		System.out.println("Inside Junit contextLoads");
+		Assert.assertEquals("Greetings from DemoController!",demoController.index());
 	}
 
 }
